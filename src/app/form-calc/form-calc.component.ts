b8510a75ff9e9c9dcf413f09fe349128fdfe0d70
Isabela Bueno <isa.belabu@hotmail.com>
2024-03-10 22:33:38 -0300

    novas funcionalidades e estilização da calculadora

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -6,11 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './form-calc.component.css'
 })
 export class FormCalcComponent {
+  operacao: string = "";
   num1: number = 0;
   num2: number = 0;
-  sum: number = 0;
+  result: number = 0;
 
-  onClickSum(){
-    this.sum = this.num1 + this.num2;
+  onClick(){
+    if(this.operacao == "soma"){
+      this.result = this.num1 + this.num2;
+    } else if(this.operacao == "subtracao"){
+      this.result = this.num1 - this.num2;
+    } else if(this.operacao == "multiplicacao"){
+      this.result = this.num1 * this.num2;
+    } else if(this.operacao == "divisao"){
+      this.result = this.num1 / this.num2;
+    } else if(this.operacao == "modulo"){
+      this.result = this.num1 % this.num2;
+    } else if(this.operacao == "potencia"){
+      this.result = Math.pow(this.num1, this.num2);
+    }
   }
 }
